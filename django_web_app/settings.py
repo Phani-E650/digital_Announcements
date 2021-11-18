fe7576cc09e4c185a92d9982f5b5aace97bd93a8
Phani Kumar <58501017+Phani-E650@users.noreply.github.com>
2021-11-18 21:42:31 +0530

    Update settings.py

--- django_web_app/settings.py
+++ django_web_app/settings.py
@@ -15,8 +15,8 @@ import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
